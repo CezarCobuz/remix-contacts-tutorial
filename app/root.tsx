0c8a70e8817f8aede0d5829111682bc1fb4140ae
CezarCobuz <cobuzcezar@gmail.com>
2023-11-15 13:03:22 +0200

    Add action for empty contact

--- app/root.tsx
+++ app/root.tsx
@@ -11,8 +11,8 @@ import {
   ScrollRestoration,
   useLoaderData,
 } from "@remix-run/react";
-import { getContacts } from "./data";
 import appStylesHref from "./app.css";
+import { createEmptyContact, getContacts } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
@@ -21,6 +21,11 @@ export const links: LinksFunction = () => [
 export const loader = async () => {
   const contacts = await getContacts();
   return json({ contacts });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
 };
 
 export default function App() {
