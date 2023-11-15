7726978ca987d2bebafb1e7a55625d8e2b25e518
CezarCobuz <cobuzcezar@gmail.com>
2023-11-15 16:25:18 +0200

    Make the search effects neater

--- app/root.tsx
+++ app/root.tsx
@@ -45,6 +45,10 @@ export default function App() {
 
   const submit = useSubmit();
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
   return (
     <html lang="en">
       <head>
@@ -70,8 +74,9 @@ export default function App() {
                 type="search"
                 name="q"
                 value={query}
+                className={searching ? "loading" : ""}
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!searching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
@@ -109,7 +114,9 @@ export default function App() {
         </div>
 
         <div
-          className={navigation.state === "loading" ? "loading" : ""}
+          className={
+            navigation.state === "loading" && !searching ? "loading" : ""
+          }
           id="detail"
         >
           <Outlet />
